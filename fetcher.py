3404710e2d6275a4e059813b19d418b68e4f8082
Lorenzo Carnevale <lorenzocarnevale@gmail.com>
2019-06-11 07:03:52 +0200

    store in into data folder

--- fetcher.py
+++ fetcher.py
@@ -14,6 +14,7 @@ __description__ = ''
 
 
 # standard libraries
+import os
 import sys
 import json
 import argparse
@@ -24,68 +25,67 @@ import pandas as pd
 
 
 def main():
-	"""
-	"""
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument("-c", "--credentials",
+    parser.add_argument("-c", "--credentials",
 						dest="credentials",
 						help="Credentials filename.",
 						type=str)
 
-	parser.add_argument("-H", "--hashtag",
+    parser.add_argument("-H", "--hashtag",
 						dest="hashtag",
 						help="Select the hashtag.",
 						type=str)
 
-	parser.add_argument("-l", "--lang",
+    parser.add_argument("-l", "--lang",
                         dest="lang",
                         help="Define the tweets' language to be fetched.",
                         type=str, default='en')
 
-	options = parser.parse_args()
+    options = parser.parse_args()
 
-	with open(options.credentials, 'r') as f:
-		conf = json.load(f)
-		CONSUMER_KEY = conf["consumer_key"]
-		CONSUMER_SECRET = conf["consumer_secret"]
-		ACCESS_KEY = conf["access_key"]
-		ACCESS_SECRET = conf["access_secret"]
-	OUT_DIRECTORY = '../datasets/'
+    with open(options.credentials, 'r') as f:
+        conf = json.load(f)
+        CONSUMER_KEY = conf["consumer_key"]
+        CONSUMER_SECRET = conf["consumer_secret"]
+        ACCESS_KEY = conf["access_key"]
+        ACCESS_SECRET = conf["access_secret"]
+    OUT_DIRECTORY = 'data/'
 
-	# access to Tweet APIs
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-	api = tweepy.API(auth, wait_on_rate_limit=True)
+    # access to Tweet APIs
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
 	# fetching tweets
-	columns = [
+    columns = [
 		'timestamp',
 		'text',
 		'language',
 		'username',
 		'coordinates'
 	]
-	df = pd.DataFrame(columns=columns)
-	try:
-		print('Start fetching...')
-		for status in tqdm(tweepy.Cursor(api.search, q=options.hashtag, lang=options.lang, rpp=100, tweet_mode='extended', include_rts=False).items(),unit="status"):
-			if (not status.retweeted) and ('RT @' not in status.full_text):
-				row = [
+    df = pd.DataFrame(columns=columns)
+    try:
+        print('Start fetching...')
+        for status in tqdm(tweepy.Cursor(api.search, q=options.hashtag, lang=options.lang, rpp=100, tweet_mode='extended', include_rts=False).items(),unit="status"):
+            if (not status.retweeted) and ('RT @' not in status.full_text):
+                row = [
 					status.created_at,
 					status.full_text,
 					status.lang,
 					status.user.screen_name,
-					status.coordinates
-				]
-				df.loc[status._json['id']] = row
-	except KeyboardInterrupt:
-		print("\nforced closing")
-	finally:
-
-		filename = options.hashtag+".csv"
-		df.to_csv(filename)
-		print("saved on ", filename)
+                    status.coordinates
+                ]
+            df.loc[status._json['id']] = row
+    except KeyboardInterrupt:
+        print("\nforced closing")
+    finally:
+        filename = '%s%s.csv' % (OUT_DIRECTORY, options.hashtag)
+        if not os.path.exists(OUT_DIRECTORY):
+            os.makedirs(OUT_DIRECTORY)
+        df.to_csv(filename)
+        print("saved on ", filename)
 
 
 if __name__ == '__main__':
